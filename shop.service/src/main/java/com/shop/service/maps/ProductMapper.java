9f335ec72bfb81ad8c2bbd18a377fc965cbf5ca0
TiagoMartins2 <tiago.martins@dnxt.pt>
2023-09-05 09:06:13 +0100

    Product fix mapper and put and post

--- shop.service/src/main/java/com/shop/service/maps/ProductMapper.java
+++ shop.service/src/main/java/com/shop/service/maps/ProductMapper.java
@@ -5,6 +5,8 @@ import com.shop.service.dto.ProductDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.Arrays;
+
 @Mapper
 public interface ProductMapper {
 
@@ -13,4 +15,12 @@ public interface ProductMapper {
     ProductDto productToDto(Product product);
 
     Product dtoToProduct(ProductDto productDto);
+
+    default byte[] toBytes(String string){
+        return string != null ? string.getBytes() : null;
+    }
+
+    default String toString(byte[] bytes){
+        return bytes != null ? Arrays.toString(bytes) : null;
+    }
 }
