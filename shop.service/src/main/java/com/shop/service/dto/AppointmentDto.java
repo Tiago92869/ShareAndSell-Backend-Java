a9cdee7c15321a54b73dec12b88540c5b817f055
TiagoMartins2 <tiago.martins@dnxt.pt>
2023-09-07 21:23:12 +0100

    Appointment Set up dto, mapper and controller

--- shop.service/src/main/java/com/shop/service/dto/AppointmentDto.java
+++ shop.service/src/main/java/com/shop/service/dto/AppointmentDto.java
@@ -1,4 +1,28 @@
 package com.shop.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class AppointmentDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private UUID id;
+
+    private LocalTime time;
+
+    private LocalDate date;
+
+    private UUID userId;
+
 }
--- shop.service/src/main/java/com/shop/service/services/AppointmentService.java
+++ shop.service/src/main/java/com/shop/service/services/AppointmentService.java
@@ -1,8 +1,13 @@
 package com.shop.service.services;
 
+import com.shop.service.dto.AppointmentDto;
 import com.shop.service.repositories.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class AppointmentService {
@@ -12,5 +17,30 @@ public class AppointmentService {
     @Autowired
     public AppointmentService(AppointmentRepository appointmentRepository) {
         this.appointmentRepository = appointmentRepository;
+    }
+
+    public Page<AppointmentDto> getAllAppointments(Pageable pageable) {
+
+        return null;
+    }
+
+    public AppointmentDto getAppointmentById(UUID id) {
+
+        return null;
+    }
+
+    public AppointmentDto createAppointment(AppointmentDto appointmentDto) {
+
+        return null;
+    }
+
+    public AppointmentDto updateAppointment(UUID id, AppointmentDto appointmentDto) {
+
+        return null;
+    }
+
+    public void deleteAppointment(UUID id) {
+
+
     }
 }
