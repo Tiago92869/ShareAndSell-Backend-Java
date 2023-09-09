841ae7c881bb5e3b57aa48dd7e5bdb98675161da
TiagoMartins2 <tiago.martins@dnxt.pt>
2023-09-09 23:57:11 +0100

    Add filters to shopProduct

--- shop.service/src/main/java/com/shop/service/repositories/ShopProductRepository.java
+++ shop.service/src/main/java/com/shop/service/repositories/ShopProductRepository.java
@@ -1,9 +1,23 @@
 package com.shop.service.repositories;
 
 import com.shop.service.domain.ShopProduct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ShopProductRepository extends JpaRepository<ShopProduct, UUID> {
+    Page<ShopProduct> findByShopId(Pageable pageable, UUID shopId);
+
+    Page<ShopProduct> findByProductId(Pageable pageable, UUID productId);
+
+    Page<ShopProduct> findByShopIdAndProductId(Pageable pageable, UUID shopId, UUID productId);
+
+    Page<ShopProduct> findByShopIdAndIsEnable(Pageable pageable, UUID shopId, Boolean isEnable);
+
+    Page<ShopProduct> findByProductIdAndIsEnable(Pageable pageable, UUID productId, Boolean isEnable);
+
+    Page<ShopProduct> findByShopIdAndProductIdAndIsEnable(Pageable pageable, UUID shopId, UUID productId, Boolean isEnable);
 }
