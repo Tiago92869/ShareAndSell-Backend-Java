50749a3808d9b1bd102774347d959ee99df3efcf
TiagoMartins2 <tiago.martins@dnxt.pt>
2023-09-11 02:08:19 +0100

    Log Service set up service and controller

--- logs.service/src/main/java/com/logs/service/controller/LogController.java
+++ logs.service/src/main/java/com/logs/service/controller/LogController.java
@@ -1,11 +1,54 @@
 package com.logs.service.controller;
 
+import com.logs.service.domain.Log;
+import com.logs.service.dto.LogDto;
+import com.logs.service.service.LogService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
 
 @Tag(name = "Logs", description = "Manage Logs")
 @RequestMapping("/log")
 @RestController
 public class LogController {
+
+    private final LogService logService;
+
+    @Autowired
+    public LogController(LogService logService) {
+        this.logService = logService;
+    }
+
+
+    @GetMapping("/")
+    @Operation(summary = "Get all Logs")
+    @ResponseStatus(HttpStatus.OK)
+    public List<LogDto> getAllLogs(){
+
+        return this.logService.getAllLogs();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get Log by id")
+    @ResponseStatus(HttpStatus.OK)
+    public LogDto getLogById(@PathVariable UUID id){
+
+        return this.logService.getLogById(id);
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Create Log")
+    @ResponseStatus(HttpStatus.OK)
+    public LogDto createLog(@RequestBody LogDto logDto){
+
+        return this.logService.createLog(logDto);
+    }
 }
--- logs.service/src/main/java/com/logs/service/service/LogService.java
+++ logs.service/src/main/java/com/logs/service/service/LogService.java
@@ -1,7 +1,53 @@
 package com.logs.service.service;
 
+import com.logs.service.domain.Log;
+import com.logs.service.dto.LogDto;
+import com.logs.service.exceptions.EntityNotFoundException;
+import com.logs.service.mapper.LogMapper;
+import com.logs.service.repository.LogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 
 @Service
 public class LogService {
+
+    private final LogRepository logRepository;
+
+    @Autowired
+    public LogService(LogRepository logRepository) {
+        this.logRepository = logRepository;
+    }
+
+    public List<LogDto> getAllLogs() {
+
+        return this.logRepository.findAll().stream().map(LogMapper.INSTANCE::logToDto).toList();
+    }
+
+    public LogDto getLogById(UUID id) {
+
+        Optional<Log> optionalLog = this.logRepository.findById(id);
+
+        if(optionalLog.isEmpty()){
+            throw new EntityNotFoundException("A Log with that id could not be found");
+        }
+
+        return LogMapper.INSTANCE.logToDto(optionalLog.get());
+    }
+
+    public LogDto createLog(LogDto logDto) {
+
+        logDto.setId(UUID.randomUUID());
+
+        Log log = LogMapper.INSTANCE.dtoToLog(logDto);
+
+        return LogMapper.INSTANCE.logToDto(this.logRepository.save(log));
+    }
 }
