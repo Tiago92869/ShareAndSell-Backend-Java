71f985c5a7528dc13b53fec5ea1f46bca79aea6b
Tiago-Martins-Augentic <tiago.martins@augentic.com>
2023-09-18 22:03:51 +0100

    Set up User Service

---
+++ user.service/src/main/java/com/user/service/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.user.service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User Service")
+                        .version("1.0"));
+    }
+
+    static {
+        var schema = new Schema<LocalTime>();
+        schema.example(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        SpringDocUtils.getConfig().replaceWithSchema(LocalTime.class, schema);
+    }
+}
