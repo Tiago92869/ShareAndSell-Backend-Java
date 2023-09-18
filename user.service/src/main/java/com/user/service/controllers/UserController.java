09d4d9153db2faa3982bac595860d2b44ecadf79
Tiago-Martins-Augentic <tiago.martins@augentic.com>
2023-09-18 23:20:58 +0100

    Set up User Controller and Service

--- user.service/src/main/java/com/user/service/controllers/UserController.java
+++ user.service/src/main/java/com/user/service/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.user.service.controllers;
 
+import com.user.service.dto.UserDto;
 import com.user.service.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Tag(name = "User", description = "Manage System Users")
 @RequestMapping("/user")
@@ -16,5 +22,45 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/")
+    @Operation(description = "")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UserDto> getAllUsers(Pageable pageable){
+
+        return this.userService.getAllUsers(pageable);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(description = "")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto getUserById(@PathVariable UUID id){
+
+        return this.userService.getUserById(id);
+    }
+
+    @PostMapping("/")
+    @Operation(description = "")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto createUser(@RequestBody UserDto userDto){
+
+        return this.userService.createUser(userDto);
+    }
+
+    @PatchMapping("/{id}")
+    @Operation(description = "")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto updateUser(@PathVariable UUID id, @RequestBody UserDto userDto){
+
+        return this.userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(description = "")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@PathVariable UUID id){
+
+        this.userService.deleteUser(id);
     }
 }
