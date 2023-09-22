27d132302d53b85e75201988f70feaf18cf5e3a4
Tiago92869 <tiagoamartins02@gmail.com>
2023-09-22 20:52:58 +0100

    Implement other services (swagger not working)

--- gateway.service/src/main/java/com/gateway/service/Application.java
+++ gateway.service/src/main/java/com/gateway/service/Application.java
@@ -1,13 +1,27 @@
 package com.gateway.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config()
+				.commonTags(Arrays.asList(Tag.of("project", "MedEvac"),
+						Tag.of("application", "Gateway Service"),
+						Tag.of("service_name", "medevac-gateway")));
 	}
 
 }
